51a4641e49365254e3d4ee6f64be701b99c27518
Renee <rchar20@gmail.com>
2016-09-22 09:44:23 -1000

    WIP will scrap spec files

--- memoization.js
+++ memoization.js
@@ -1,7 +1,24 @@
 module.exports = function() {
 
+	let cache = {};
+
+
+	function addElementById(elementId) {
+		cache[elementId] = elementId;
+	}
+
+	function retrieveElementById(elementId) {
+		return cache[elementId];
+	}
 
 	return {
-		
+		findElementById: function (elementId) {
+			if(cache.hasOwnProperty(elementId)) {
+				retrieveElementById(elementId);
+			} else {
+				addElementById(elementId);
+				return document.getElementById(elementId);
+			}
+		}
 	}
 }
--- test/memoization.spec.js
+++ test/memoization.spec.js
@@ -13,4 +13,24 @@ describe('memoization file', () => {
 	it('should return an object', () => {
 		expect(memoization()).to.be.an('object');
 	});
+
+	describe('findElement()', () => {
+		beforeEach(() => {
+			let theRoot = document.getElementById('root');
+			newCache = memoization();
+			let brain = document.createElement('div');
+			brain.Id = 'brain';
+			theRoot.appendChild(brain);
+		});
+
+		it('should be a function', () => {
+			expect(memoization.findElement).to.be.a('function');
+		});
+		it('should return the element if it is in the cache', ()=> {
+			expect(memoization.findElement())
+		});
+		it.skip('should add the element to the cache if it is not in the cache', () => {
+			expect()
+		});
+	})
 })
